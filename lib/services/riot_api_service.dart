18c6b6e78cd633ffdc873c62af4bdae4530a5b01
yohan <beginnerdevyohan@gmail.com>
2026-02-02 20:37:22 -0500

    Recent Matches Update 2

--- lib/services/riot_api_service.dart
+++ lib/services/riot_api_service.dart
@@ -9,51 +9,7 @@ import '../models/champion_name.dart';
 
 
 class RiotApiService {
-  Future<PlayerData> fetchPlayerData(String userName, String userTag) async { 
-    final response = await http.get(Uri.parse('https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/$userName/$userTag?api_key=${dotenv.env['API_KEY']}'),
-    );
-
-    if (response.statusCode == 200) {
-      return PlayerData.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load data: ${response.statusCode}');
-    }
-  }
-
-  Future<MatchId> fetchMatchIds(String puuid) async {
-    final response = await http.get(Uri.parse('https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/$puuid/ids?start=0&count=20&api_key=${dotenv.env['API_KEY']}'),
-    );
-
-    if (response.statusCode == 200) {
-      return MatchId.fromJson(jsonDecode(response.body));
-    } else  {
-      throw Exception('Failed to load match IDs: ${response.statusCode}');
-    }
-  }
-
-  Future<MatchId> fetchMatchIdExample(String userName, String userTag) async {
-    final accountResponse = await http.get(Uri.parse('https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/$userName/$userTag?api_key=${dotenv.env['API_KEY']}'),
-    );
-
-    if (accountResponse.statusCode != 200) {
-      throw Exception('Failed to load account data: ${accountResponse.statusCode}');
-    }
-
-    final playerData = PlayerData.fromJson(jsonDecode(accountResponse.body));
-    final playerUniqueId = playerData.puuid;
-
-    final matchResponse = await http.get(Uri.parse('https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/$playerUniqueId/ids?start=0&count=20&api_key=${dotenv.env['API_KEY']}'),
-    );
-
-    if (matchResponse.statusCode == 200) {
-      final List<dynamic> jsonresponse = jsonDecode(matchResponse.body);
-
-      return MatchId(listMatch: jsonresponse.cast<String>());
-    } else {
-      throw Exception('Failed to load match IDs: ${matchResponse.statusCode}');
-    }
-  }
-
+  
   Future<List<ChampionName>> fetchChampionNameEX(String userName, String userTag) async {
     final accountResponse = await http.get(Uri.parse('https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/$userName/$userTag?api_key=${dotenv.env['API_KEY']}'),
     );
@@ -75,9 +31,6 @@ class RiotApiService {
 
     final List<dynamic> matchIds = jsonDecode(matchResponse.body);
     
-    for (int i = 0; i < matchIds.length; i++) {
-      print(matchIds[i]);
-    }
     final List<ChampionName> matchData = [];
 
     for (int i = 0; i < matchIds.length; i++ ) {
@@ -103,32 +56,6 @@ class RiotApiService {
     }
     return matchData;
   }
-    // final firstMatchID = matchIds[0];
 
-    // final matchDetailResponse = await http.get(Uri.parse('https://americas.api.riotgames.com/lol/match/v5/matches/$firstMatchID?api_key=${dotenv.env['API_KEY']}'));
-
-    // if (matchDetailResponse.statusCode == 200) {
-    //   final matchDetailJson = jsonDecode(matchDetailResponse.body);
-    //   final participants = matchDetailJson['info']['participants'] as List<dynamic>;
-
-    //   for (var participant in participants) {
-    //     if (participant['puuid'] == playerUniqueId) {
-    //       final championName = participant['championName'];
-    //       final kills = participant['kills'];
-    //       final deaths = participant['deaths'];
-    //       final assists = participant['assists'];
-    //       final win = participant['win'];
-    //       return ChampionName(
-    //         championName: championName,
-    //         kills: kills,
-    //         deaths: deaths,
-    //         assists: assists,
-    //         win: win,
-    //       );
-    //     }
-    //   }
-    //   throw Exception('Champion not found for the player in the match.');
-    // } else {
-    //   throw Exception('Failed to load match details: ${matchDetailResponse.statusCode}');
-    // }
-  }
+  Future
+}
